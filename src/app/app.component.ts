b9c3075b3d8b79640dfdfcc60dc2835d85ddd951
kejvinGL <kejvinbraka@gmail.com>
2024-10-03 12:06:16 +0200

    lint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,45 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit } from "@angular/core";
+import { RouterOutlet } from "@angular/router";
+import { CurrentUserService } from "./shared/services/current-user.service";
+import { FooterComponent } from "./shared/components/footer/footer.component";
+import { HeaderComponent } from "./shared/components/header/header.component";
+import { AuthService } from "./shared/services/auth.service";
+import { SingleComponent } from "./shared/components/single/single.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: "app-root",
+	standalone: true,
+	imports: [RouterOutlet, HeaderComponent, FooterComponent, SingleComponent],
+	template: `
+		@if (authService.getCurrentUser() !== null) {
+			<app-header></app-header>
+			<router-outlet></router-outlet>
+			@defer (on viewport) {
+				<app-footer></app-footer>
+			} @placeholder {
+				<div></div>
+			}
+			@defer {
+				<app-single></app-single>
+			}
+		} @else {
+			<router-outlet></router-outlet>
+		}
+	`,
+	styles: `
+		app-header {
+			z-index: 3;
+		}
+		app-footer {
+			margin-top: 5rem;
+		}
+	`,
 })
-export class AppComponent {
-  title = 'netflix-clone';
+export class AppComponent implements OnInit {
+	authService = inject(AuthService);
+
+	constructor(private currentUserService: CurrentUserService) {}
+	ngOnInit() {
+		// this.currentUserService.setCurrentUser();
+	}
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,37 @@
-import { Routes } from '@angular/router';
+import { Routes } from "@angular/router";
+import { LoginComponent } from "./features/login/login.component";
+import { AuthGuardService } from "./shared/services/auth-guard.service";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+	{
+		path: "login",
+		component: LoginComponent,
+	},
+
+	{
+		path: "browse",
+		loadComponent: () =>
+			import("./features/browse/browse.component").then(
+				(m) => m.BrowseComponent,
+			),
+		canActivate: [AuthGuardService],
+	},
+	{
+		path: "search",
+		loadComponent: () =>
+			import("./features/search/search.component").then(
+				(m) => m.SearchComponent,
+			),
+		canActivate: [AuthGuardService],
+	},
+	// {
+	// 	path: "single",
+	// 	loadComponent: () =>
+	// 		import("./features/single/single.component").then(
+	// 			(m) => m.SingleComponent,
+	// 		),
+	// 	canActivate: [AuthGuardService],
+	// },
+
+	{ path: "**", redirectTo: "login" },
+];
